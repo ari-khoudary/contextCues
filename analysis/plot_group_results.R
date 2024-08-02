f263332abf8b3af577d68127e1127ee2da62cd6e
Maria Khoudary <makhouda@uci.edu>
2024-08-02 10:19:57 -0700

    added code for bar plotting

--- analysis/plot_group_results.R
+++ analysis/plot_group_results.R
@@ -14,7 +14,7 @@ nSubs <- length(unique(inferenceData_tidy$subID))
 
 ###### plot group summary statistics ###### 
 ## make a summary data frame
-inferenceSummary <- inferenceData_tidy %>% 
+inferenceSummary_cue <- inferenceData_tidy %>% 
   group_by(cueLevel) %>%
   summarise(propCorrect = mean(accuracy, na.rm=T),
             propCorrect_sem = (sd(accuracy, na.rm=T) / sqrt(nSubs)),
@@ -26,7 +26,7 @@ inferenceSummary <- inferenceData_tidy %>%
             confRT_sem = sd(zlogconfRT)/sqrt(nSubs))
 
 ## plot group-level accuracy as a function of cue level
-inferenceSummary %>%
+inferenceSummary_cue %>%
   ggplot(aes(x=cueLevel, y=propCorrect)) + # specify what you want on x and y axes
   ylim(c(0, 1)) + # specify your y limits
   scale_x_continuous(breaks=c(0.2, 0.5, 0.8)) + # tell the x-axis where we want labels
@@ -39,7 +39,7 @@ inferenceSummary %>%
 ggsave(paste0(outdir, 'accuracy.png'), width=3, height=4, dpi='retina') # save the current figure 
 
 ## plot group-level RTs as a function of cue level
-inferenceSummary %>%
+inferenceSummary_cue %>%
   ggplot(aes(x=cueLevel, y=RT)) +
   geom_hline(yintercept=0, linewidth=0.15) + 
   geom_pointrange(aes(ymin = RT-RT_sem, ymax=RT+RT_sem)) + 
@@ -50,38 +50,110 @@ inferenceSummary %>%
   theme(plot.title = element_text(hjust=0.5)) # center the title 
 ggsave(paste0(outdir, 'RT.png'), width=3, height=4, dpi='retina') # save the current figure 
 
-## plot RT as a function of accuracy & cue level
-inferenceData_tidy %>%
-  group_by(accuracy, congruent, cueLevel) %>%
-  summarise(meanRT = mean(zlogRT),
-            sem = sd(zlogRT) / sqrt(nSubs)) %>%
-  mutate(accuracy = factor(accuracy, levels=c(0,1), labels=c('incorrect','correct'))) %>%
-  ggplot(aes(x=cueLevel, y=meanRT, group=accuracy, color=accuracy)) +
-  geom_hline(yintercept=0, linewidth=0.15) + 
-  geom_pointrange(aes(ymin = meanRT - sem, ymax = meanRT + sem)) +
-  geom_line() +
-  theme_classic() +
-  labs(y = 'RT (log-transformed and z-scored within subject)', title='RT as a function of accuracy and cue reliability') +
-  theme(plot.title = element_text(hjust=0.5)) # center the title 
-ggsave(paste0(outdir, 'speedAccuracy.png'), width=t, height=4, dpi='retina') # save the current figure 
-
-## plot confidence as a function of accuracy & cue level
-inferenceData_tidy %>%
+## make a dataframe to plot RT and confidence as a function of trial-accuracy & cue level
+inferenceSummary_acc <- inferenceData_tidy %>%
   group_by(subID) %>%
   mutate(zconf = scale(confResp),
-         accuracy = factor(accuracy, levels=c(0,1), labels=c('incorrect', 'correct'))) %>%
+         accuracy = factor(accuracy, levels=c(1,0), labels=c('correct', 'incorrect'))) %>%
+  ungroup() %>%
   group_by(accuracy, congruent, cueLevel) %>%
-  summarise(meanRT = mean(zconf),
-            sem = sd(zconf) / sqrt(nSubs)) %>%
-  ggplot(aes(x=cueLevel, y=meanRT, group=accuracy,color=accuracy)) +
+  summarise(meanRT = mean(zlogRT),
+            RT_sem = sd(zlogRT) / sqrt(nSubs),
+            meanZConf = mean(zconf),
+            ZConf_sem = sd(zconf) / sqrt(nSubs)) 
+
+## plot RT as a function of accuracy & cue level
+inferenceSummary_acc %>%
+  ggplot(aes(x=cueLevel, y=meanRT, group=accuracy, color=accuracy)) +
   geom_hline(yintercept=0, linewidth=0.15) + 
-  geom_pointrange(aes(ymin = meanRT - sem, ymax = meanRT + sem)) +
+  geom_pointrange(aes(ymin = meanRT - RT_sem, ymax = meanRT + RT_sem)) +
+  geom_line() +
+  theme_classic() +
+  scale_x_continuous(breaks=c(0.2, 0.5, 0.8)) +
+  scale_color_manual(values = c('forestgreen', 'darkmagenta'))+
+  labs(y = 'RT (log-transformed and z-scored within subject)', title='RT as a function of accuracy and cue reliability') +
+  theme(plot.title = element_text(hjust=0.5)) # center the title 
+ggsave(paste0(outdir, 'speedAccuracy.png'), width=6, height=4, dpi='retina') # save the current figure 
+
+## plot confidence as a function of accuracy & cue level
+inferenceSummary_acc %>%
+  ggplot(aes(x=cueLevel, y=meanZConf, group=accuracy,color=accuracy)) +
+  geom_hline(yintercept=0, linewidth=0.15) + 
+  geom_pointrange(aes(ymin = meanZConf - ZConf_sem, ymax = meanZConf + ZConf_sem)) +
   geom_line() +
   theme_classic() + 
+  scale_x_continuous(breaks=c(0.2, 0.5, 0.8)) +
+  scale_color_manual(values = c('forestgreen', 'darkmagenta')) +
   labs(y = 'confidence (z-scored within subject)', title = 'Decision confidence as a function of accuracy and cue reliability') +
   theme(plot.title = element_text(hjust=0.5)) # center the title 
 ggsave(paste0(outdir, 'speedConfidence.png'), width=6, height=4, dpi='retina')
 
+########### bar plot versions ########### 
+# make some changes to inferenceData_tidy to make the plots we want
+inferenceData_tidy <- inferenceData_tidy %>%
+  mutate(trueCue = ifelse(cueLevel==0.2, 0.8, cueLevel),
+         congruent = factor(congruent, levels=c('neutral', 'congruent', 'incongruent')))
+
+inferenceSummary_bar <- inferenceData_tidy %>%
+  group_by(subID) %>%
+  mutate(zconf = scale(confResp)) %>%
+  ungroup() %>%
+  group_by(subID, trueCue, congruent) %>%
+  summarise(propCorrect = mean(accuracy),
+            meanRT = mean(zlogRT),
+            meanZConf = mean(zconf)) %>%
+  mutate(trueCue = factor(trueCue, levels=c(0.5, 0.8), labels=c('50% cue', '80% cue')))
+
+groupSummary_bar <- inferenceData_tidy %>%
+  group_by(subID) %>%
+  mutate(zconf = scale(confResp)) %>%
+  ungroup() %>%
+  group_by(trueCue, congruent) %>%
+  summarise(propCorrect = mean(accuracy),
+            accuracy_sem = sd(accuracy) / sqrt(nSubs),
+            meanRT = mean(zlogRT),
+            RT_sem = sd(zlogRT) / sqrt(nSubs),
+            meanZConf = mean(zconf),
+            ZConf_sem = sd(zconf)/sqrt(nSubs)) %>%
+  mutate(trueCue = factor(trueCue, levels=c(0.5, 0.8), labels=c('50% cue', '80% cue')))
+
+ggplot(groupSummary_bar, aes(x=trueCue, y=propCorrect, fill=congruent)) +
+  ylim(0,1) +
+  theme_classic() + 
+  geom_hline(yintercept=0.5, linewidth=0.15) +
+  geom_col(position = position_dodge(width=0.25), width = 0.25) +
+  geom_errorbar(aes(ymin=propCorrect - accuracy_sem, ymax = propCorrect + accuracy_sem), width=0.1, 
+                position = position_dodge(width=0.25)) +
+  geom_point(size=4, shape=8, color='gray20',position = position_dodge(width=0.25) ) +
+  geom_point(aes(color=congruent), data=inferenceSummary_bar, position=position_jitterdodge(dodge.width=0.25, jitter.width = 0.1), 
+             color='gray20', shape=21, size=2, alpha=0.75)
+
+
+
+
+
+
+
+
+
+inferenceData_tidy$congruent <- factor(inferenceData_tidy$congruent, levels=c('neutral', 'congruent', 'incongruent'))
+inferenceSummary_cong <- inferenceData_tidy %>% 
+  group_by(congruent) %>%
+  summarise(propCorrect = mean(accuracy),
+            sem = sd(accuracy)/sqrt(nSubs),
+            meanRT = mean(zlogRT),
+            RT_sem = sd(zlogRT)/sqrt(nSubs),
+            meanConf = mean(confResp),
+            conf_sem = sd(confResp)/sqrt(nSubs))
+  
+inferenceSummary_sub <- inferenceData_tidy %>%
+  group_by(subID, congruent) %>%
+  summarise(propCorrect = mean(accuracy),
+            meanRT = mean(zlogRT),
+            meanConf = mean(confResp))
+
+ggplot(inferenceSummary_cong, aes(x=congruent, y=propCorrect)) +
+  geom_col()
 
 ########### let's get distributional (plotting RT distributions) ########### 
 inferenceData_tidy %>%
