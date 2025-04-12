38daeb61b448cd2d44e7d4b0e3b40b47f1355165
Maria Khoudary <makhouda@uci.edu>
2025-04-11 20:10:48 -0700

    add functionality of compiling and writing out all data

--- analysis/scripts/tidyData.R
+++ analysis/scripts/tidyData.R
@@ -1,15 +1,23 @@
 ### loads, tidies, and outputs aggregated & tidied dataframes for both conditions
+
+#### setup ####
 library(tidyverse)
 
-# Create a vector of condition names
+nFrames = 240 # based on matlab expt details
+# create lists to store concatenated datasets
+calibration_all <- list()
+cohVal_all <- list()
+learning_all <- list()
+estimates_all <- list()
+inference_all <- list()
+
+# define condition dirs
 conditions <- c("condition_80cue", "condition_65cue")
+all_outdir <- '../tidied_data/all_conditions/'
 
-# specify nFrames based on matlab expt code
-nFrames = 240
-
-# Process each condition
+# tidy data for each condition
 for (cond_string in conditions) {
-  # Set cueValue based on condition
+  # set cueValue based on condition
   cueValue <- ifelse(cond_string == "condition_80cue", 80, 65)
   
   # define directory to write tidied data
@@ -18,8 +26,7 @@ for (cond_string in conditions) {
   # define directory to read in data from
   indir <- paste0('../../data/', cond_string)
   
-  
-  #### calibration data ####
+  #### process calibration data ####
   # load
   calibration_files = list.files(indir, full.names = TRUE, pattern='calibration.csv', recursive = T)
   calibration_keep_idx <- grep('excluded', calibration_files, invert=T)
@@ -33,11 +40,14 @@ for (cond_string in conditions) {
     mutate(zlogRT = scale(log(RT))) %>% 
     ungroup()
   
+  # store
+  calibration_all[[cond_string]] <- calibration_df
+  
   # write
   write.csv(calibration_df, paste0(outdir, 'calibration.csv'), row.names = F)
   
   
-  #### coherence validation ####
+  #### process coherence validation data ####
   # load
   cohVal_files = list.files(indir, full.names = TRUE, pattern='coherenceValidation.csv', recursive = T)
   calibration_keep_idx <- grep('excluded', cohVal_files, invert=T)
@@ -66,15 +76,18 @@ for (cond_string in conditions) {
     group_by(subID) %>%
     mutate(zlogRT = scale(log(RT)))
   
+  # store
+  cohVal_all[[cond_string]] <- cohVal_df
+  
   # write
   write.csv(cohVal_df, paste0(outdir, 'coherenceValidation.csv'), row.names = F)
   
-  #### cue learning ####
+  #### process cue learning data ####
   learning_files = list.files(indir, full.names = TRUE, pattern='learning.csv', recursive = T)
   learning_keep_idx <- grep('excluded', learning_files, invert=T)
   learning_df = do.call(rbind, lapply(learning_files[learning_keep_idx], function(x) { read.csv(x, header = TRUE)} )) 
   
-  # Only run this block for 80cue condition
+  # only run this block for 80cue condition
   if (cond_string == "condition_80cue") {
     # load second round of learning data (if applicable)
     learning2_files = list.files(indir, full.names = TRUE, pattern='learning2.csv', recursive = T)
@@ -96,15 +109,18 @@ for (cond_string in conditions) {
   # tidy
   learning_df <- learning_df %>%
     mutate(congruent = factor(congruent, levels=c("0", "1", "NaN"), labels=c('incongruent', 'congruent', 'neutral')),
-           trueCue = case_when(congruent != 'neutral' ~ cueValue,
+           trueCue = case_when(congruent != 'neutral' ~ cueValue/100,
                                congruent == 'neutral' ~ 0.5),
            congCue = ifelse(congruent=='incongruent', 1-trueCue, trueCue),
            imgLockedRT = RT - cueDuration)
   
+  # store
+  learning_all[[cond_string]] <- learning_df
+  
   # write out 
   write.csv(learning_df, paste0(outdir, 'learning.csv'), row.names = F)
   
-  #### learning validation ####
+  #### process learning validation data ####
   # load
   learnVal_files = list.files(indir, full.names = TRUE, pattern='learningValidation.csv', recursive = T)
   learnVal_keep_idx <- grep('excluded', learnVal_files, invert=T)
@@ -117,7 +133,7 @@ for (cond_string in conditions) {
     rename(cueName = cueString,
            cueConfidence = confidence)
   
-  # tidy up / add relevant variables to estimate
+  # add new variables
   estimates <- learnVal_df %>%
     select(c(subID, block, cueName, cueIdx, congImgIdx, RT, sliderValue, cueConfidence, confRT, trueCue)) %>%
     mutate(imgIdx_subjective = case_when(cueIdx==1 & sliderValue < 50 | cueIdx==2 & sliderValue > 50 | cueIdx==3 & sliderValue==50 ~ congImgIdx,
@@ -133,6 +149,9 @@ for (cond_string in conditions) {
                                          congImgIdx==2 ~ sliderValue),
            subjectiveCue = subjectiveCue / 100,
            subjectiveCongCue = subjectiveCongCue / 100)
+  
+  # store
+  estimates_all[[cond_string]] <- estimates
 
   # write out tidied data
   write.csv(estimates, paste0(outdir, 'learningValidation.csv'), row.names = F)
@@ -143,7 +162,7 @@ for (cond_string in conditions) {
   inference_keep_idx <- grep('excluded', inference_files, invert=T)
   inference_df = do.call(rbind, lapply(inference_files[inference_keep_idx], function(x) { read.csv(x, header = TRUE)} ))
   
-  # Prepare estimates for joining with inference data
+  # prepare estimates for joining with inference data
   estimates <- estimates %>% 
     select(c(subID, block, cueName, cueIdx, cueConfidence, imgIdx_subjective, subjectiveCue, subjectiveCongCue))
   
@@ -174,6 +193,16 @@ for (cond_string in conditions) {
              cueConfidence, imgIdx_subjective, subjectiveCue, subjectiveCongCue, trueCue, congCue, noise1_duration, signal1_onset, noise2_onset, noise2_duration,
              signal2_onset, signal2_duration, zlogRT))
   
+  # store
+  inference_all[[cond_string]] <- inference_df
+  
   # write out tidied inference data
   write.csv(inference_df, paste0(outdir, 'inference.csv'), row.names = F)
 }
+
+# Combine and write out combined data files
+write.csv(do.call(rbind, calibration_all), paste0(all_outdir, 'calibration.csv'), row.names = F)
+write.csv(do.call(rbind, cohVal_all), paste0(all_outdir, 'coherenceValidation.csv'), row.names = F)
+write.csv(do.call(rbind, learning_all), paste0(all_outdir, 'learning.csv'), row.names = F)
+write.csv(do.call(rbind, estimates_all), paste0(all_outdir, 'estimates.csv'), row.names = F)
+write.csv(do.call(rbind, inference_all), paste0(all_outdir, 'inference.csv'), row.names = F)
